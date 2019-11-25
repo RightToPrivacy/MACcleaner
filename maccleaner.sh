cb6dc0164a45352f4f7be71fed2d3454cedba26a
RightToPrivacy <58191050+RightToPrivacy@users.noreply.github.com>
2019-11-25 16:29:25 -0500

    Update maccleaner.sh

--- maccleaner.sh
+++ maccleaner.sh
@@ -3,7 +3,7 @@
 # prevent MAC change pattern/location analysis/identification/tracking;
 # MAC address gets reset to original on disconnect/reset of 
 # wifi; this script ensures MAC continues changing after such incident.
-# requires macchanger; Contact me: devurandom@protonmail.com 
+# requires macchanger; Contact me: righttoprivacy@tutanota.com
 
 mac=wlan0 	# your wifi card goes here
 while :
