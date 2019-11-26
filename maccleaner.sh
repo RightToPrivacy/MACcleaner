986ddd75a663f1c2b3c3378beb1b2db2423ad9e0
RightToPrivacy <58191050+RightToPrivacy@users.noreply.github.com>
2019-11-26 17:26:14 -0500

    Update maccleaner.sh
    fixed the printout to be more accurate. Have some other ideas I may be combining into this project.

--- maccleaner.sh
+++ maccleaner.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Simple script to create random mac address, at random sec intervals to 
-# prevent MAC change pattern/location analysis/identification/access point
-# linkage tracking;
+# prevent MAC change pattern/location analysis/identification/tracking;
 # requires macchanger; jme0719a@harford.edu 
 
-mac=wlan0 	# wifi card goes here
-hightime=1000   # set highest number of seconds between next random MAC change time
-lowtime=1       # set lowest number of seconds between next random MAC change time
+mac=wlan0       # wifi card goes here
+hightime=1000   # highest number of seconds between next random MAC change time?
+lowtime=1       # lowest number of seconds between next random MAC change time?
 while :
 do
-	rando=$((RANDOM%$hightime+$lowtime))
-	sleep $rando
-	echo 'MAC changed after:' $rando'sec'
-	ifconfig $mac down;macchanger -r $mac;ifconfig $mac up
+        echo 'ghostmac generating random times, random addresses:'
+        rando=$((RANDOM%$hightime+$lowtime))
+        ifconfig $mac down;macchanger -r $mac;ifconfig $mac up
+        echo 'MAC changing in:' $rando'sec'
+        sleep $rando
 done
