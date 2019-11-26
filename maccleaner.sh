1907ddc6c56f87b45cef4b0798b42998ab377c34
RightToPrivacy <58191050+RightToPrivacy@users.noreply.github.com>
2019-11-25 19:31:34 -0500

    Update maccleaner.sh

--- maccleaner.sh
+++ maccleaner.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Simple script changes MAC addy at random sec intervals to 
 # thwart MAC pattern/location analysis/identification/tracking;
-# MAC addresses are mapped out w/drones to track ppl;
-# MAC addresses tend to reset to original trackable MAC on disconnect/reset of 
-# wifi; this script ensures MAC continues changing after such incident.
-# requires macchanger; Contact righttoprivacy@tutanota.com
+# MAC addresses are mapped out w/drones/access points to track MAC locations;
+# MAC addresses tend to reset to original permanent trackable MAC on disconnect/reset of 
+# wifi; this script continues MAC changing after such incident.
+# requires macchanger; righttoprivacy@tutanota.com
 
 mac=wlan0 	# your wifi card goes here
 while :
 do
-	rando=$((RANDOM%100+60))
+	rando=$((RANDOM%500+200))	# change numbers to change random time frames between random changes
 	sleep $rando
-	echo 'MAC changed after:' $rando'sec'
+	echo 'MAC changed after:' $rando'sec'     # lists last random change time frame
 	ifconfig $mac down;macchanger -r $mac;ifconfig $mac up
 done
