2819c286eabf08c997d3f0d1b64cb7c1975fb157
RightToPrivacy <58191050+RightToPrivacy@users.noreply.github.com>
2019-11-26 14:08:51 -0500

    Update maccleaner.sh
    I added variables to make it easier to read/set. Edited the README to include instructions for adding a button launcher on MATE desktop (you can do same on KDE/XFCE).

--- maccleaner.sh
+++ maccleaner.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# Simple script changes MAC addy at random sec intervals to 
-# thwart MAC pattern/location analysis/identification/tracking;
-# MAC addresses are mapped out w/drones/access points to track MAC locations;
-# MAC addresses tend to reset to original permanent trackable MAC on disconnect/reset of 
-# wifi; this script continues MAC changing after such incident.
-# requires macchanger; righttoprivacy@tutanota.com
+# Simple script to create random mac address, at random sec intervals to 
+# prevent MAC change pattern/location analysis/identification/access point
+# linkage tracking;
+# requires macchanger; jme0719a@harford.edu 
 
-mac=wlan0 	# your wifi card goes here
+mac=wlan0 	# wifi card goes here
+hightime=1000   # set highest number of seconds between next random MAC change time
+lowtime=1       # set lowest number of seconds between next random MAC change time
 while :
 do
-	rando=$((RANDOM%500+200))	# change numbers to change random time frames between random changes
+	rando=$((RANDOM%$hightime+$lowtime))
 	sleep $rando
-	echo 'MAC changed after:' $rando'sec'     # lists last random change time frame
+	echo 'MAC changed after:' $rando'sec'
 	ifconfig $mac down;macchanger -r $mac;ifconfig $mac up
 done
