596467af292a7bd9bcb1dec1d5bef4d5864443f6
RightToPrivacy <58191050+RightToPrivacy@users.noreply.github.com>
2019-11-27 04:05:34 -0500

    Add files via upload
    Now allows input from the user to set their wifi device name (usually wlan0). 
    Added permanent MAC feature that continues ensuring it is set to avoid wifi card address resets/leaks.

--- maccleaner.sh
+++ maccleaner.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
-# Simple script to create random mac address, at random sec intervals to 
-# prevent MAC change pattern/location analysis/identification/tracking;
-# requires macchanger; jme0719a@harford.edu 
+# wifi cards commonly reset depending on task/driver
+# maccleaner sets MAC address on a timed basis to prevent broadcast leaks
+# questions/concept: righttoprivacy@tutanota.com
+# requires macchanger
 
-mac=wlan0       # wifi card goes here
-hightime=1000   # highest number of seconds between next random MAC change time?
-lowtime=1       # lowest number of seconds between next random MAC change time?
-while :
-do
-        echo 'ghostmac generating random times, random addresses:'
-        rando=$((RANDOM%$hightime+$lowtime))
-        ifconfig $mac down;macchanger -r $mac;ifconfig $mac up
-        echo 'MAC changing in:' $rando'sec'
-        sleep $rando
-done
+hightime=1000  # highest number of seconds between next random MAC change time
+lowtime=1   # lowest number of seconds between next random MAC change time
+
+echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+echo "|maccleaner:Prevents MAC address reset leaks problem|"
+echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+echo "promotes privacy with randomized mac change timing setting"
+echo "press 'r wlan0' to enter random mode (times/addresses)..."
+echo "press 's wlan0' to create permanent MAC address setting mode..."
+echo "setting device:"
+read setting device
+
+if [ "$setting" == "r" ];then
+
+	while :
+	do
+		rando=$((RANDOM%$hightime+$lowtime))
+		echo 'ghosting MAC: random times at random addresses (privacy mode):'
+		ifconfig $device down;macchanger -r $device;ifconfig $device up
+		echo 'MAC changing in:' $rando'sec'
+		sleep $rando
+	done
+fi
+
+
+if [ "$setting" == "s" ];then
+	echo "Set the address (and help prevent reset address leaks)"
+	echo "MAC:";read mac
+		while :
+		do
+			ifconfig $device down;macchanger -m $mac $device;ifconfig $device up > /dev/null 2>&1 &
+			echo "Perfect!"
+			sleep 600
+		done
+
+fi 
